dfddcc096c29d32c470fcec9b884b149934724e8
trantbatey <trantbatey@gmail.com>
2020-04-08 17:51:14 -0500

    Completed Part One: TDD exercise.

---
+++ src/main/java/Student.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private long id;
+    private static long count;
+    private String name;
+    public List<Integer> grades;
+
+    public Student() {
+        count++;
+        id = count;
+        grades = new ArrayList<>();
+    }
+
+    public Student(String name) {
+        this();
+        this.name = name;
+    }
+
+    public static long getCount() {
+        return count;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addGrade(int i) {
+        grades.add(i);
+    }
+
+    public int getGradeAverage() {
+        int sum = 0;
+        for(int grade : grades) {
+            sum += grade;
+        }
+        return sum/grades.size();
+    }
+
+    public void deleteGrade(int index) {
+        grades.remove(index);
+    }
+
+    public int getGrade(int index) {
+        return grades.get(index);
+    }
+
+    public void updateGrade(int index, int newGrade) {
+        grades.remove(index);
+        grades.add(index, newGrade);
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,93 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    Student studentOne;
+
+    @Before
+    public void setUP() {
+        studentOne = new Student("Vivian");
+        studentOne.addGrade(100);
+        studentOne.addGrade(95);
+        studentOne.addGrade(90);
+        studentOne.addGrade(93);
+        studentOne.addGrade(87);
+    }
+
+    @Test
+    public void testIfConstructorWorked() {
+        Student student = new Student();
+        assertNotNull(student);
+    }
+
+    @Test
+    public void testCountIsWorking() {
+        long expected = Student.getCount();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        expected += 2;
+        assertEquals(expected, Student.getCount());
+    }
+
+    @Test
+    public void testIfIdIsInitailized() {
+        long count = Student.getCount();
+        Student s1 = new Student();
+        assertEquals("Test ID 1", count + 1, s1.getId());
+    }
+
+    @Test
+    public void testIfIStudentNameIsSetByConstructor() {
+        long count = Student.getCount();
+        String expected = "Trant";
+        Student student = new Student(expected);
+        assertEquals("Test ID 1", expected, student.getName());
+    }
+
+    @Test
+    public void testSettingAndGettingTheName() {
+        long count = Student.getCount();
+        String expected = "Trant";
+        Student student = new Student();
+        student.setName(expected);
+        assertEquals("Test ID 1", expected, student.getName());
+    }
+
+    @Test
+    public void testAddingGradesAndAveraging() {
+        Student student = new Student("Casey");
+        student.addGrade(100);
+        student.addGrade(95);
+        student.addGrade(90);
+        int expected = 95;
+        assertEquals("Testing Grade Average ", 95, student.getGradeAverage());
+    }
+
+    @Test
+    public void testGetGrade() {
+        int expected = 90;
+        int actual = studentOne.getGrade(2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeleteGrade() {
+        studentOne.deleteGrade(2);
+        int expected = 93;
+        int actual = studentOne.getGrade(2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testUpdateGrade() {
+        int expected = 97;
+        studentOne.updateGrade(2, expected);
+        int actual = studentOne.getGrade(2);
+        assertEquals(expected, actual);
+    }
+}
